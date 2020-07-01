51588f3a8480f25326a754994e6a87fdd6057fe7
Matthew Kim <matty-kim@atlassian.com>
2020-07-01 03:05:24 -0400

    Updates

---
+++ precache-manifest.a3ab051636f5077487ece34307e90d31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e9014740f08c47b9ea2c003a2468fb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b1cd86f96da37490bcc",
+    "url": "/static/css/main.f5c26fb2.chunk.css"
+  },
+  {
+    "revision": "d315c159344beb33ce74",
+    "url": "/static/js/2.6cb9b678.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.6cb9b678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1cd86f96da37490bcc",
+    "url": "/static/js/main.e28aa380.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "6fe14f62e8ec5f37a5b145d8ac89b197",
+    "url": "/static/media/education.6fe14f62.png"
+  },
+  {
+    "revision": "8ec877bedd99415ba5f4313686206143",
+    "url": "/static/media/experience.8ec877be.png"
+  },
+  {
+    "revision": "bf0bb784f84d477b54684b8a4b123128",
+    "url": "/static/media/profile.bf0bb784.png"
+  }
+]);
